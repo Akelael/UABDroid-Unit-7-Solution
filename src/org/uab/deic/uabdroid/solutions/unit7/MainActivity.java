57487e6db0c8ca9ff19a36847eb92a7c6a2602ba
Rubén Serrano <akelael@gmail.com>
2012-05-14 08:37:27 +0200

    Versio 1.0

---
+++ src/org/uab/deic/uabdroid/solutions/unit7/MainActivity.java
@@ -0,0 +1,14 @@
+package org.uab.deic.uabdroid.solutions.unit7;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    public void onCreate(Bundle _savedInstanceState) 
+    {
+        super.onCreate(_savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
